1bf27a22ec1312d63612668e65a9e80ae56452ad
Keith <kmiron@gmail.com>
2021-12-04 13:06:35 -0800

    Copy to clipboard without ssl

--- scripts/track.js
+++ scripts/track.js
@@ -2149,7 +2149,28 @@ function ResetTimer()
 function CopyTimerToClipboard()
 {
 	var text = document.getElementById('timerDiv').textContent;
-	navigator.clipboard.writeText(text.substring(0,text.length-3));
+
+	// navigator clipboard api needs a secure context (https)
+    if (navigator.clipboard && window.isSecureContext) {
+        // navigator clipboard api method'
+        return navigator.clipboard.writeText(text.substring(0,text.length-3));
+    } else {
+        // text area method
+        let textArea = document.createElement("textarea");
+        textArea.value = text.substring(0,text.length-3);
+        // make the textarea out of viewport
+        textArea.style.position = "fixed";
+        textArea.style.left = "-999999px";
+        textArea.style.top = "-999999px";
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+        return new Promise((res, rej) => {
+            // here the magic happens
+            document.execCommand('copy') ? res() : rej();
+            textArea.remove();
+        });
+    }
 }
 
 function SwapKeyItemLocation(locationId) {
