2f6072831c45a91ba1a4df76116e4dea997c4253
Dustin Tanksley <dt7c9@mst.edu>
2022-02-08 04:21:47 -0600

    Setting up branch

---
+++ AutoTrack.lua
@@ -0,0 +1,30 @@
+local socket = require("socket.core")
+
+tcp=socket.tcp()
+tcp:connect('127.0.0.1',54321)
+tcp:setoption('keepalive',true)
+
+local function checkKIs()
+	local lowLOC = memory.readdword(0x7E1514)
+	local highLOC = memory.readdword(0x7E1518)
+	local KI = bit.band(memory.readdword(0x7E1500),0x1FFFF)
+	tcp:send(string.format("{\"KI\": %d,\"Loc1\": %d,\"Loc2\": %d}",KI,lowLOC,highLOC))
+end
+
+local function myframe()
+	if(emu.framecount() % 300 == 0) then
+		checkKIs()
+	end
+end
+
+local function metaData()
+	local numBytes=memory.readdword(0x3FF000)
+	local str = ""
+	for i=1,numBytes-2 do
+		str = str .. string.char(memory.readbyte(0x3FF004+i))
+	end
+	return string.format("\"metadata\": {\n%s}\n",str)
+end
+
+emu.registerbefore(myframe)
+tcp:send("{" .. metaData() .. "}")
---
+++ AutoTrack.py
@@ -0,0 +1,59 @@
+import asyncio
+import websockets
+import json
+import threading
+import socketserver
+
+connected = set()
+globe = {}
+
+async def handle_client(reader, writer):
+    request = None
+    global globe
+    global connected
+    while request != 'quit':
+        request = (await reader.read(1024)).decode('utf8')
+        print(request)
+        x=json.loads(request)
+        print(json.dumps(x, sort_keys=True, indent=4))
+        if 'metadata' in x:
+            globe=x
+        for ws in connected:
+            try:
+                await ws.send(json.dumps(x, sort_keys=True, indent=4))
+            except:
+                print("Error, a connection was dropped")
+    writer.close()
+
+async def run_server():
+    server = await asyncio.start_server(handle_client, 'localhost', 54321)
+    async with server:
+        await server.serve_forever()
+
+async def handler(websocket, path):
+    global connected
+    global globe
+    connected.add(websocket)
+    try:
+        await websocket.send(json.dumps(globe, sort_keys=True, indent=4))
+        while True:
+            name = await websocket.recv()
+            print("< {}".format(name))
+            
+    except:
+        connected.remove(websocket)
+        
+def Thread1():
+    asyncio.run(run_server())
+    
+
+x = threading.Thread(target=Thread1)
+x.start()
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+
+start_server = websockets.serve(handler, 'localhost', 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
