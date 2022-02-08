e44adea1d13a295356c8f2d882e967d9e8c71073
simbu95 <simbu.tanksley@yahoo.com>
2022-02-08 02:27:53 -0600

    Update snes9x.js

--- scripts/snes9x.js
+++ scripts/snes9x.js
@@ -38,9 +38,30 @@ function snes9x() {
                 console.log('Message from server ', event.data);
                 dat=JSON.parse(event.data)
                 if(dat['metadata']){
-                    flags=dat['metadata']['flags']
-                    objectives=dat['metadata']['objectives']
+                    flags=dat['metadata']['flags'].toUpperCase()
+                    Objectives=dat['metadata']['objectives']
                     SetModes();
+                    ApplyChecks();
+                }
+                else if(dat['KI']){
+                    out = [dat['KI'] & 1 << b and 1 for b in range(17)]
+                    order = [3,9,6,7,10,5,8,4,0,13,1,11,12,2,14,15,16]
+                    KI = [out[i] for i in order]
+                    for(x in KI){
+                        if(KI[x] == 1 && keyitems[x]==0){	
+                            keyitems[x] = KI[x];	
+                        }	
+                    }	
+                    out1 = [dat['Loc1'] & 1 << b and 1 for b in range(32)]
+                    out2 = [dat['Loc2'] & 1 << b and 1 for b in range(32)]
+                    out = out1 + out2
+                    order = [14,1,5,19,4,2,15,16,7,57,3,8,6,11,13,17,18,9,10,12,20,21,23,27,22,25,24]
+                    Locs = [out[i] for i in order]
+                    for(x in Locs){	
+                        if(Locs[x] == 1 ){	
+                          keyitemlocations[x] = 2;	
+                        }	
+                    }
                     ApplyChecks();
                 }
             });
