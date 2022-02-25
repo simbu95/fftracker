39049dc78113c5d5ffb40745e472fec95afc9b12
simbu95 <simbu.tanksley@yahoo.com>
2022-02-25 14:44:54 -0600

    Update tracking_interface.js

--- scripts/tracking_interface.js
+++ scripts/tracking_interface.js
@@ -1,7 +1,13 @@
 let network_kis;
 
 function getConnected() {
+  var x = getParameterByName('p')
+  if(x ==null){
   network_kis = create_network(console, isReact=false);
+  }
+  else{
+    network_kis = create_network_mecha(console, x, isReact=false);
+  }
   network_kis.onConnect().then(
     () => { console.log("Connected") },
     () => {console.log("Failure") }
