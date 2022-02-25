80b2cbb75785c11fd2c7199bf457d219d61880af
simbu95 <simbu.tanksley@yahoo.com>
2022-02-25 15:02:56 -0600

    Update tracking_interface.js

--- scripts/tracking_interface.js
+++ scripts/tracking_interface.js
@@ -1,86 +1,150 @@
 let network_kis;
+let ki_map = {
+  0x00: KeyItem.PACKAGE,
+  0x01: KeyItem.SANDRUBY,
+  0x02: KeyItem.LEGEND,
+  0x03: KeyItem.BARON_KEY,
+  0x04: KeyItem.TWINHARP,
+  0x05: KeyItem.EARTH_CRYSTAL,
+  0x06: KeyItem.MAGMA_KEY,
+  0x07: KeyItem.TOWER_KEY,
+  0x08: KeyItem.HOOK,
+  0x09: KeyItem.LUCA_KEY,
+  0x0A: KeyItem.DARKNESS_CRYSTAL,
+  0x0B: KeyItem.RAT_TAIL,
+  0x0C: KeyItem.ADAMANT,
+  0x0D: KeyItem.PAN,
+  0x0E: KeyItem.SPOON,
+  0x0F: KeyItem.PINK_TAIL,
+  0x10: KeyItem.CRYSTAL
+}
+
+let ki_location_map = {
+  0x002E: KeyItemCheck.ADAMANT,
+  0x0021: KeyItemCheck.ANTLION,
+  0x0025: KeyItemCheck.BARON_KING,
+  0x0024: KeyItemCheck.BARON_INN,
+  0x0033: KeyItemCheck.BARON_ODIN,
+  0x0022: KeyItemCheck.FABUL_DEFEND,
+  0x002F: KeyItemCheck.FABUL_SYLPH,
+  0x0030: KeyItemCheck.FABUL_PAN,
+  0x0027: KeyItemCheck.MAGNES,
+  0x0059: KeyItemCheck.MIST,
+  0x0023: KeyItemCheck.MT_ORDEALS,
+  0x0026: KeyItemCheck.TOROIA,
+  0x0028: KeyItemCheck.TOWER_ZOT,
+  0x002B: KeyItemCheck.DWARF,
+  0x002D: KeyItemCheck.FEY_CHEST,
+  0x0031: KeyItemCheck.FEY_ASURA,
+  0x0032: KeyItemCheck.FEY_LEVIATHAN,
+  0x0029: KeyItemCheck.LOWER_BABIL_BOSS,
+  0x002A: KeyItemCheck.LOWER_BABIL_CANNON,
+  0x002C: KeyItemCheck.SEALED_CAVE,
+  0x0034: KeyItemCheck.SYLPH_CAVE,
+  0x0035: KeyItemCheck.BAHAMUT,
+  0x0037: KeyItemCheck.MOON_CRYSTAL,
+  0x0036: KeyItemCheck.MOON_MURASAME,
+  0x003B: KeyItemCheck.MOON_MASAMUNE,
+  0x0039: KeyItemCheck.MOON_RIBBON,
+  0x003A: KeyItemCheck.MOON_RIBBON,
+  0x0038: KeyItemCheck.MOON_WHITE
+}
 
 function getConnected() {
-  var x = getParameterByName('p')
+  var x = getParameterByName('p');
   if(x ==null){
-  network_kis = create_network(console, isReact=false);
+    network_kis = create_network(console, isReact=false);
+    network_kis.onConnect().then(
+      () => { console.log("Connected") },
+      () => {console.log("Failure") }
+    );
   }
   else{
+    autotrackingport=x;
     network_kis = create_network_mecha(console, x, isReact=false);
     network_kis.onConnect().then(
       () => { this.timerID = setInterval( keep_updating_kis, 100) },
       () => {console.log("Failure") }
     );
   }
-  network_kis.onConnect().then(
-    () => { console.log("Connected") },
-    () => {console.log("Failure") }
-  );
+  
 }
 
 function disconnect() {
   network_kis.disconnect();
 }
 
-  module.keep_updating_kis = keep_updating_kis
-  function keep_updating_kis() {
-    network_kis.snes.send(JSON.stringify({
-       "Opcode" : "GetAddress",
-       "Space" : "SNES",
-       "Operands": ["0xF51500", "20"]
-    })).then(
-      (event_ki) => {
-       return event_ki.data.arrayBuffer()
-     }).then(
-       (ab_ki) => {
-         let memory_ki = new Uint8Array(ab_ki);
-         for (let i = 0; i <= 2; i++) {
-            for (let b = 0; b < 8; b++) {
-              let index = (i * 8 + b);
-              if (index > 0x10) continue;
-              let truth = !!(memory_ki[i] & (1 << b));
-              set_ki(index, truth);
-            }
+function keep_updating_kis() {
+  network_kis.snes.send(JSON.stringify({
+     "Opcode" : "GetAddress",
+     "Space" : "SNES",
+     "Operands": ["0xF51500", "20"]
+  })).then(
+    (event_ki) => {
+     return event_ki.data.arrayBuffer()
+   }).then(
+     (ab_ki) => {
+       let memory_ki = new Uint8Array(ab_ki);
+       for (let i = 0; i <= 2; i++) {
+          for (let b = 0; b < 8; b++) {
+            let index = (i * 8 + b);
+            if (index > 0x10) continue;
+            let truth = !!(memory_ki[i] & (1 << b));
+            set_ki(index, truth);
           }
-          for (let i = 3; i <= 5; i++) {
-             for (let b = 0; b < 8; b++) {
-               let index = (i * 8 + b) - 24;
-               if (index > (0x10)) continue;
-               let truth = !!(memory_ki[i] & (1 << b));
-               set_used_ki(index, truth);
-             }
+        }
+        /*for (let i = 3; i <= 5; i++) {
+           for (let b = 0; b < 8; b++) {
+             let index = (i * 8 + b) - 24;
+             if (index > (0x10)) continue;
+             let truth = !!(memory_ki[i] & (1 << b));
+             set_used_ki(index, truth);
            }
-           for (let i = 0x14; i <= 0x1B; i++) {
-             for (let b = 0; b < 8; b++) {
-               let index = (i * 8 + b) - 0x14*8;
-               if (index > (0x5D)) continue;
-               let truth = !!(memory_ki[i] & (1 << b));
-               set_loc_ki(index + 0x20, truth);
-             }
+         }*/
+         for (let i = 0x14; i <= 0x1B; i++) {
+           for (let b = 0; b < 8; b++) {
+             let index = (i * 8 + b) - 0x14*8;
+             if (index > (0x5D)) continue;
+             let truth = !!(memory_ki[i] & (1 << b));
+             set_loc_ki(index + 0x20, truth);
            }
-           module.auto_update_func()
-     },
-     (err) => { /* console.log("bleh" + err) */ })
-   }
+         }
+         auto_update_func()
+   },
+   (err) => { /* console.log("bleh" + err) */ })
+  }
 
-   module.auto_set_ki = (a,b) => {}
-   module.set_ki = set_ki
-    function set_ki(index, truth=True) {
-      module.auto_set_ki(index, truth);
-      //console.log("ki:" + index + ":" + truth);
-    }
-    module.auto_set_used_ki = (a,b) => {}
-    module.set_used_ki = set_used_ki
-    function set_used_ki(index, truth=True) {
-      module.auto_set_used_ki(index, truth);
-      //console.log("uki:" + index + ":" + truth);
-    }
-    module.auto_set_loc_ki = (a,b) => {}
-    module.set_loc_ki = set_loc_ki
-    function set_loc_ki(index, truth=True) {
-      module.auto_set_loc_ki(index, truth);
-      //console.log("lki:" + index + ":" + truth);
-    }
-
-    return module;
+function auto_set_ki(slot, value){
+    keyitems[ki_map[slot]] = value;
 }
+
+function set_ki(index, truth=True) {
+  auto_set_ki(index, truth);
+  //console.log("ki:" + index + ":" + truth);
+}
+
+function set_used_ki(index, truth=True) {
+  auto_set_used_ki(index, truth);
+  //console.log("uki:" + index + ":" + truth);
+}
+function auto_set_loc_ki(slot, value){
+    if (ki_location_map[slot] !== undefined) {
+      if (keyitemlocations[ki_location_map[slot]] !== 0 &&
+        keyitemlocations[ki_location_map[slot]] !== 3) {
+          if (value)
+            keyitemlocations[ki_location_map[slot]] = 2;
+          else
+            keyitemlocations[ki_location_map[slot]] = 1;
+        }
+      }
+      else {
+
+      }
+    }
+}
+
+function set_loc_ki(index, truth=True) {
+  auto_set_loc_ki(index, truth);
+  //console.log("lki:" + index + ":" + truth);
+}
+
